cd22d13502e081ba2ec3a65ac35117d8c789edf0
chen-luke <72319988+chen-luke@users.noreply.github.com>
2026-02-09 16:58:03 -0500

    updated the playwright test yml file to call the correct commands and use the correct ports

--- playwright.config.js
+++ playwright.config.js
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test'
 
 /**
  * Read environment variables from file.
@@ -28,7 +28,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
-
+    baseURL: 'http://localhost:8080',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
@@ -72,10 +72,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
-});
-
+  webServer: {
+    command: 'npm run start',
+    url: 'http://localhost:8080',
+    reuseExistingServer: !process.env.CI,
+  },
+})
